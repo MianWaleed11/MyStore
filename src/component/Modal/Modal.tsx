d7a6083004e968038fb58f22a6f9a22b313a8aff
awaismehr <awaismehr001@gmail.com>
2021-01-15 10:09:59 +0500

    modal added

---
+++ src/component/Modal/Modal.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import './Modal.css';
+import { Button, Modal } from "react-bootstrap";
+export interface CartModalProps {
+  show: boolean;
+  handleClose:
+    | ((event: React.MouseEvent<HTMLElement, MouseEvent>) => void)
+    | undefined;
+  productImage: string;
+}
+
+const CartModal: React.FC<CartModalProps> = (props) => {
+  const [isopen, setisopen] = useState(false);
+
+  return (
+    <>
+      <Modal show={props.show} onHide={props.handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className='modal_cart_image'>
+            <p>Added to Cart</p>
+            <img
+              src={props.productImage}
+              alt="product"
+              style={{ width: "50px", height: "50px" }}
+            />
+          </div>
+          <div className='subTotal'>
+            <p> SubTotal:Subtotal here </p>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={props.handleClose}>
+            Cart
+          </Button>
+          <Button variant="warning" onClick={props.handleClose}>
+            Proceed To Checkout
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default CartModal;
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import "react-placeholder/lib/reactPlaceholder.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
